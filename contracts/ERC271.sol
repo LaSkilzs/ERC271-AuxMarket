b6ee50046ef00bedd50a154fe72463c359c8eb12
La Fountain <github email address>
2019-08-08 16:00:31 -0400

    Finish all basic testing

--- contracts/ERC271.sol
+++ contracts/ERC271.sol
@@ -13,56 +13,61 @@ contract ERC271 {
   mapping(uint256 => address) public ownerOf;  // find address of tokenOwner by tokenId
   mapping(address => uint256[]) internal ownedTokens;  // who owns what token by address
 
-  mapping(uint256 => address) internal tokenApprovals; // address of users approved token use
-  mapping(address =>  mapping(address => bool)) private _operatorApprovals; //owner to operator approvals
+  mapping(uint256 => address) public tokenApprovals; // address of users approved token use
+  mapping(address =>  mapping(address => bool)) internal _operatorApprovals; //owner to operator approvals
+
   constructor(string memory _name, string memory _symbol) public{
     name = _name;
     symbol = _symbol;
     admin = msg.sender;
-    tokenId = allTokens.length;
+    tokenId += 1;
     ownerOf[tokenId] = msg.sender;
     balanceOf[msg.sender] += totalSupply;
   }
 
-  function approve(address _approved, uint256 _tokenId) external payable{
+  event Approval(address indexed _owner, address indexed _approved, uint256 indexed _tokenId);
+  event Transfer(address indexed _from, address indexed _to, uint256 _tokenId);
+  event ApprovalForAll(address indexed _sender, address indexed _to, bool _result);
+
+  function approve(address _approved, uint256 _tokenId) external payable {
     address owner = ownerOf[_tokenId];
     require(owner != _approved, 'owner cannot be the approved address');
     tokenApprovals[_tokenId] = _approved;
-    // boolean value or something to identify that function was accurate
+    emit Approval(owner, _approved, _tokenId);
   }
 
   function setApprovalForAll(address _to, bool approved) public {
     require(_to != msg.sender, 'sender and recipient can not be the same');
-    _operatorApprovals[msg.sender][_to] = approved;
-    // emit ApprovalForAll(msg.sender, _to, approved);
+    _operatorApprovals[msg.sender][_to];
+    emit ApprovalForAll(msg.sender, _to, approved);
   }
 
   function isApprovedForAll(address owner, address operator) public view returns(bool){
-    return _operatorApprovals[owner][operator];
+    // return _operatorApprovals[owner][operator];
   }
 
   function getApproved(uint256 _tokenId) public view returns(address){
-    // require(_exists(tokenId), 'a tokenId must exist');
+    require(_tokenId != 0, 'a tokenId must exist');
     return tokenApprovals[tokenId];
   }
 
-
-  function transferFrom(address _from, address _to, uint256 _tokenId) public payable {
+  function transferFrom(address _from, address _to, uint256 _tokenId) external payable {
     require(_to != address(0), 'must be a valid address to send to');
     require(_from != _to, 'can not transfer token to same address');
     require(ownerOf[_tokenId] != address(0), 'sender must be approved');
-    // remove token from one address
-    // add token to another address
-    // emit Transfer event
+    ownerOf[_tokenId] = _to;
+    balanceOf[_to] += 1;
+    balanceOf[_from] -= 1;
+    emit Transfer(_from, _to, _tokenId);
+  }
 
-  }
   function safeTransferFrom(address _from, address _to, uint256 _tokenId, bytes calldata data) external payable{
-    transferFrom(_from, _to, _tokenId);
-    // require(_checkOnERC721Received(_from, _to, _tokenId, data), 'not a safe function to perform');
+    // transferFrom(_from, _to, _tokenId);
+   
   }
+
   function safeTransferFrom(address _from, address _to, uint256 _tokenId) external payable{
-    transferFrom(_from, _to, _tokenId);
-    // require(_checkOnERC721Received(_from, _to, _tokenId, data), 'not a safe function to perform');
+    // transferFrom(_from, _to, _tokenId);
   }
 
 
--- test/approval_test.js
+++ test/approval_test.js
@@ -4,12 +4,38 @@ contract("ERC271", accounts => {
   let tokenInstance;
   owner = accounts[0];
   approvedOwner = accounts[1];
-  return Token.deployed().then(instance => {
-    tokenInstance = instance;
-    return tokenInstance.approve.call(accounts[1], 0);
+  it("should approve accounts", () => {
+    return Token.deployed()
+      .then(instance => {
+        tokenInstance = instance;
+        return tokenInstance.approve.call(accounts[1], 1);
+      })
+      .then(success => {
+        return tokenInstance.approve(accounts[1], 1);
+      })
+      .then(receipt => {
+        assert.equal(receipt.logs.length, 1, "triggers event");
+        assert.equal(
+          receipt.logs[0].event,
+          "Approval",
+          "triggers approval event"
+        );
+        assert.equal(receipt.logs[0].args._owner, owner, "triggers event");
+        assert.equal(
+          receipt.logs[0].args._approved,
+          approvedOwner,
+          "owner not approved"
+        );
+        assert.equal(
+          receipt.logs[0].args._tokenId,
+          1,
+          "tokenId is not what is expected"
+        );
+        return tokenInstance.tokenApprovals(1);
+      })
+
+      .then(address => {
+        assert.equal(address, approvedOwner, "account was not approved");
+      });
   });
-  // .then(receipt => {
-  //   return tokenInstance.tokenApprovals[]
-  //   console.log(receipt);
-  // });
 });
